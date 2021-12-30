816f87b973282700114ad6b8245996a690cbf1bf
BastianHarttung <info@harttung-fotografie.de>
2021-12-30 11:08:08 +0100

    add tooltip to add user btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 //Components
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -28,7 +29,8 @@ import { UserComponent } from './user/user.component';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
